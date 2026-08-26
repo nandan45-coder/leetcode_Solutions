440bc50ee8210b549bc391b90c00dc5e011ef61f
Nandkishor K <nandankhobrekar45@gmail.com>
2026-08-26 19:50:30 +0530

    Time: 91 ms (91.65%), Space: 16.7 MB (46.35%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices, fee):
+        cash = 0
+        hold = -prices[0]
+
+        for price in prices:
+            cash = max(cash, hold + price - fee)
+            hold = max(hold, cash - price)
+
+        return cash
