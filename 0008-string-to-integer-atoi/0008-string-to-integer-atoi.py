02ce404fec050d5df7e517bc25ec7f10d720e62d
Nandkishor K <nandankhobrekar45@gmail.com>
2026-09-04 09:41:22 +0530

    Time: 3 ms (61.19%), Space: 12.4 MB (59.13%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,27 @@
+class Solution:
+    def myAtoi(self, s):
+        i = 0
+        n = len(s)
+        sign = 1
+        result = 0
+
+        while i < n and s[i] == ' ':
+            i += 1
+
+        if i < n and (s[i] == '+' or s[i] == '-'):
+            if s[i] == '-':
+                sign = -1
+            i += 1
+
+        while i < n and s[i].isdigit():
+            digit = int(s[i])
+
+            if result > (2**31 - 1 - digit) // 10:
+                if sign == 1:
+                    return 2**31 - 1
+                return -2**31
+
+            result = result * 10 + digit
+            i += 1
+
+        return sign * result
